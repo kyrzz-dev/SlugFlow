ebd8920dfdf475cccce990f342bbdddc81e0c433
k4yr2 <kyrzkaya@gmail.com>
2024-12-12 13:14:31 +0300

    Added state method that matches source

--- lib/slug/base.ts
+++ lib/slug/base.ts
@@ -1,11 +1,35 @@
 import SlugSource from "./source";
 import SlugState from "../slugState";
-import { toSlugs } from "../util/slugFormat";
+import { toRest, toSlugs } from "../util/slugFormat";
 
 abstract class SlugBase {
-
     protected abstract target() : SlugState;
 
+    public getState(src : SlugSource) : SlugState | null {
+        const slugs = toSlugs(src);
+    
+        if(slugs[0] == ':') {
+            throw new Error("Invalid slug: Root path cannot begin with ':'");
+        }
+    
+        const target = this.target();
+        const hierarchy = target.hierarchy;
+        const content = hierarchy.getContent();
+        const isLast = slugs.length == 1;
+    
+        for(const child of content){
+            if(child.name == slugs[0]){
+                
+                if(isLast) {
+                    return child;
+                }
+
+                return child.getState(toRest(slugs));
+            }
+        }
+
+        return null;
+    }
 }
 
 export default SlugBase;
