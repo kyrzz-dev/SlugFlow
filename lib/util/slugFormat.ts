4d39e993fbbb02abdef48befb6dc32c97dd3d0f6
k4yr2 <kyrzkaya@gmail.com>
2024-12-12 11:03:55 +0300

    Added src overload for format

--- lib/util/slugFormat.ts
+++ lib/util/slugFormat.ts
@@ -2,6 +2,8 @@ function toSlugs(path : string) : string[];
 function toSlugs(path : string, s : string) : string[];
 function toSlugs(slugs : string[]) : string[];
 function toSlugs(slugs : string[], s : string) : string[];
+function toSlugs(src : string | string[]) : string[];
+function toSlugs(src : string | string[], s : string) : string[];
 function toSlugs(src : string | string[], s : string = '/') : string[] {
     if(Array.isArray(src)){
         return src;
@@ -18,11 +20,13 @@ function toPath(path : string) : string;
 function toPath(path : string, s : string) : string;
 function toPath(slugs : string[]) : string;
 function toPath(slugs : string[], s : string) : string;
+function toPath(src : string | string[]) : string;
+function toPath(src : string | string[], s : string) : string;
 function toPath(src : string | string[], s: string = '/') : string {
     if(!Array.isArray(src)){
         return src;
     }
-    
+
     if(s.length != 1) {
         throw new Error();
     }
