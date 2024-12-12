5ddad9a4212c241cbc39825940236499b752b643
k4yr2 <kyrzkaya@gmail.com>
2024-12-12 12:33:15 +0300

    SlugSource implementation

--- lib/util/slugFormat.ts
+++ lib/util/slugFormat.ts
@@ -1,10 +1,12 @@
+import SlugSource from "../slug/source";
+
 function toSlugs(path : string) : string[];
 function toSlugs(path : string, s : string) : string[];
 function toSlugs(slugs : string[]) : string[];
 function toSlugs(slugs : string[], s : string) : string[];
-function toSlugs(src : string | string[]) : string[];
-function toSlugs(src : string | string[], s : string) : string[];
-function toSlugs(src : string | string[], s : string = '/') : string[] {
+function toSlugs(src : SlugSource) : string[];
+function toSlugs(src : SlugSource, s : string) : string[];
+function toSlugs(src : SlugSource, s : string = '/') : string[] {
     if(Array.isArray(src)){
         return src;
     }
@@ -20,9 +22,9 @@ function toPath(path : string) : string;
 function toPath(path : string, s : string) : string;
 function toPath(slugs : string[]) : string;
 function toPath(slugs : string[], s : string) : string;
-function toPath(src : string | string[]) : string;
-function toPath(src : string | string[], s : string) : string;
-function toPath(src : string | string[], s: string = '/') : string {
+function toPath(src : SlugSource) : string;
+function toPath(src : SlugSource, s : string) : string;
+function toPath(src : SlugSource, s: string = '/') : string {
     if(!Array.isArray(src)){
         return src;
     }
@@ -38,9 +40,9 @@ function toRest(path : string) : string[];
 function toRest(path : string, i : number) : string[];
 function toRest(slugs : string[]) : string[];
 function toRest(slugs : string[], i : number) : string[];
-function toRest(src : string | string[]) : string[];
-function toRest(src : string | string[], i : number) : string[];
-function toRest(src : string | string[], i : number = 1) : string[] {
+function toRest(src : SlugSource) : string[];
+function toRest(src : SlugSource, i : number) : string[];
+function toRest(src : SlugSource, i : number = 1) : string[] {
     return toSlugs(src).slice(i);
 }
 
@@ -48,9 +50,9 @@ function toSource(path : string) : string[];
 function toSource(path : string, i : number) : string[];
 function toSource(slugs : string[]) : string[];
 function toSource(slugs : string[], i : number) : string[];
-function toSource(src : string | string[]) : string[];
-function toSource(src : string | string[], i : number) : string[];
-function toSource(src : string | string[], i : number = -1) : string[] {
+function toSource(src : SlugSource) : string[];
+function toSource(src : SlugSource, i : number) : string[];
+function toSource(src : SlugSource, i : number = -1) : string[] {
     return toSlugs(src).slice(0, i);
 }
 
